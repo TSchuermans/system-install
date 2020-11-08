62c10606eaade7b4af39b5489f9bb1a3aaa4c621
Tom Schuermans <t.schuermans@gmail.com>
2020-11-08 12:46:02 +0100

    Updated system install

--- install.sh
+++ install.sh
@@ -15,45 +15,32 @@ install_homebrew() {
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 }
 
+title "Cache sudo credentials"
+sudo -v
+
 title "Install homebrew"
 [[ -x "/usr/local/bin/brew" ]] && install_homebrew || subTitle "Homebrew is already installed."
 
 title "Install homebrew packages"
 brew tap Homebrew/bundle
-echo '' > ~/.Brewfile
-echo 'tap "homebrew/bundle"' >> ~/.Brewfile
-echo 'tap "homebrew/cask"' >> ~/.Brewfile
-echo 'tap "homebrew/cask-versions"' >> ~/.Brewfile
-echo 'tap "homebrew/core"' >> ~/.Brewfile
-echo 'tap "homebrew/services"' >> ~/.Brewfile
-echo 'brew "zsh"' >> ~/.Brewfile
-echo 'brew "openssl"' >> ~/.Brewfile
-echo 'brew "wget"' >> ~/.Brewfile
-echo 'brew "curl"' >> ~/.Brewfile
-echo 'brew "git"' >> ~/.Brewfile
-echo 'brew "python"' >> ~/.Brewfile
-echo 'brew "php"' >> ~/.Brewfile
-echo 'brew "mariadb"' >> ~/.Brewfile
-echo 'brew "autojump"' >> ~/.Brewfile
-echo 'brew "telnet"' >> ~/.Brewfile
-echo 'cask "iterm2"' >> ~/.Brewfile
-echo 'cask "1password"' >> ~/.Brewfile
-echo 'cask "1password-cli"' >> ~/.Brewfile
-echo 'cask "google-chrome"' >> ~/.Brewfile
-echo 'cask "firefox"' >> ~/.Brewfile
-echo 'cask "microsoft-edge"' >> ~/.Brewfile
-echo 'cask "phpstorm"' >> ~/.Brewfile
-echo 'cask "datagrip"' >> ~/.Brewfile
-echo 'cask "visual-studio-code"' >> ~/.Brewfile
-echo 'cask "slack"' >> ~/.Brewfile
-echo 'cask "microsoft-teams"' >> ~/.Brewfile
-echo 'cask "zoomus"' >> ~/.Brewfile
-echo 'cask "contexts"' >> ~/.Brewfile
-echo 'cask "dropbox"' >> ~/.Brewfile
-echo 'cask "google-backup-and-sync"' >> ~/.Brewfile
-echo 'cask "daisydisk"' >> ~/.Brewfile
-echo 'cask "sequel-pro"' >> ~/.Brewfile
-echo 'cask "spotify"' >> ~/.Brewfile
-echo 'cask "whatsapp"' >> ~/.Brewfile
-echo 'cask "logitech-options"' >> ~/.Brewfile
+(cd && curl -O "https://raw.githubusercontent.com/TSchuermans/system-install/main/.Brewfile")
 brew bundle --global
+
+title "Install powerline fonts"
+# clone
+git clone https://github.com/powerline/fonts.git --depth=1
+# install
+cd fonts
+./install.sh
+# clean-up a bit
+cd ..
+rm -rf fonts
+
+title "Install oh-my-zsh"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sed -i '' -e 's/ZSH_THEME="robbyrussell"/ZSH_THEME="agnoster"/g' ~/.zshrc
+echo 'export PATH=$HOME/.composer/vendor/bin:$PATH' | cat - ~/.zshrc > temp && mv temp ~/.zshrc
+(cd /tmp && curl -O "https://raw.githubusercontent.com/TSchuermans/system-install/main/Solarized Dark - Patched.itermcolors" && open "/tmp/Solarized Dark - Patched.itermcolors")
+
+title "Configure osx"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/TSchuermans/system-install/main/.macos)"
